7e85ca5f3efd690f35a96ac2952196ca70d26254
jimColl <jimmattcoll@gmail.com>
2021-04-11 15:22:41 -0500

    New pointcloud attempt

---
+++ potree/pointclouds/KS_BakerWetlands/cloud.js
@@ -0,0 +1,51 @@
+{
+    "version": "1.8",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 49257165,
+    "boundingBox": {
+        "lx": 305621.0304260254,
+        "ly": 4309538.725921631,
+        "lz": 205.03883361816407,
+        "ux": 305913.9103088379,
+        "uy": 4309831.605804443,
+        "uz": 497.91871643066409
+    },
+    "tightBoundingBox": {
+        "lx": 305621.03,
+        "ly": 4309538.726,
+        "lz": 205.03900000000002,
+        "ux": 305860.313,
+        "uy": 4309831.606,
+        "uz": 224.525
+    },
+    "pointAttributes": [
+        {
+            "name": "POSITION_CARTESIAN",
+            "size": 12,
+            "elements": 3,
+            "elementSize": 4,
+            "type": "int32",
+            "description": ""
+        },
+        {
+            "name": "RGBA",
+            "size": 4,
+            "elements": 4,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "classification",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        }
+    ],
+    "spacing": 2.53641414642334,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
